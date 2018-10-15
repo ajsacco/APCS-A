a7f6f01cd212bbf42260460c360d67544792dabd
AidanS24 <saccoa54@gmail.com>
2018-10-15 10:16:04 -0700

    finish gibberish

--- Projects/Gibberish-master/Gibberish.java
+++ Projects/Gibberish-master/Gibberish.java
@@ -2,31 +2,39 @@
  * Write a description of class Gibberish here.
  *
  * @author (Aidan Sacco)
- * @version (1011)
+ * @version (1015)
  */
 public class Gibberish {
     private String sourceString;
     
     public Gibberish(){
+        //import template and create new sourceString
         Template template = new Template();
         sourceString = template.getSourceString();
     }
     
     public String makeGibberish(WordList5000 wordList5000){
+        //declare and initialize variables
         int currentSourceIndex = 0;
-        String resultString;
+        String resultString = "";
         int nextTagIndex = 0;
         String placeHolderString = "";
         while(currentSourceIndex < sourceString.length()){
+            //traverse sourceString and look for tag.
             nextTagIndex = sourceString.indexOf("<", currentSourceIndex);
             if (nextTagIndex == -1) {
-                resultString = sourceString.substring(currentSourceIndex);
+                //if tag is not found, append the rest of sourceString to resultString and exit;
+                resultString += sourceString.substring(currentSourceIndex);
+                //resultString += ".";
                 break;
             }else{
-                placeHolderString = sourceString.indexOf(">", nextTagIndex+1) - 1);
-                
+                //if tag is found, replace word inside tag with new word, then append the next section of sourceString to resultString until next tag is found
+                resultString += sourceString.substring(currentSourceIndex, nextTagIndex);
+                placeHolderString = sourceString.substring(nextTagIndex, sourceString.indexOf(">", nextTagIndex+1)-1);
+                if (wordList5000.getRandomWordString(placeHolderString) != null) resultString += wordList5000.getRandomWordString(placeHolderString);
+                currentSourceIndex = sourceString.indexOf("</>", nextTagIndex) + 3;
             }
         }
-        return "";
+        return resultString;
     }
 }
--- Projects/Gibberish-master/GibberishRunner.java
+++ Projects/Gibberish-master/GibberishRunner.java
@@ -2,15 +2,35 @@
 /**
  * Write a description of class GibberishRunner here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Aidan Sacco)
+ * @version (1015)
  */
+import javax.swing.JOptionPane;
+
 public class GibberishRunner
 {
     public static void main(){
+        //make new Gibberish object and use it to make a gibberishString, then print
         WordList5000 wordList5000 = new WordList5000();
         Gibberish gibberish = new Gibberish();
         String gibberishString = gibberish.makeGibberish(wordList5000);
-        System.out.println(gibberishString);
+        System.out.println(gibberishString + ".");
+
+    }
+
+    public static void mainWithGUI(){
+        //same as above method, except this is modified to use JOptionPane for a pretty fancy (and easy) GUI :D
+        boolean e = true;
+        while(e){
+            int b = JOptionPane.showConfirmDialog(null, "New String?", "New String?", JOptionPane.YES_NO_OPTION);
+            if (b != 0) e = false;
+            else{
+                //make new Gibberish object and use it to make a gibberishString, then print
+                WordList5000 wordList5000 = new WordList5000();
+                Gibberish gibberish = new Gibberish();
+                String gibberishString = gibberish.makeGibberish(wordList5000);
+                JOptionPane.showMessageDialog(null, gibberishString);
+            }
+        }
     }
 }
